c363084092a6ce0b5846f51fa518f5834bedb487
LaaLkA <n.afonchenkov@yandex.ru>
2024-12-05 20:50:32 +0300

    Initial commit

---
+++ src/main/java/ru/laalka/java_spring_hw_5/JavaSpringHw5Application.java
@@ -0,0 +1,13 @@
+package ru.laalka.java_spring_hw_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHw5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringHw5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/laalka/java_spring_hw_5/JavaSpringHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.laalka.java_spring_hw_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringHw5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
